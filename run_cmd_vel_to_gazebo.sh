709f92f8a36873f21e84b1f2a4be182f4dde8df1
Jiatian Yuan <yuan.jiatian@gmail.com>
2026-04-28 22:17:12 -0500

    practiced collecting rosbags, added odometry topics

--- run_cmd_vel_to_gazebo.sh
+++ run_cmd_vel_to_gazebo.sh
@@ -46,16 +46,16 @@ sleep 1 # Wait a second for ports to establish
 # Window 2: Gazebo Simulator
 gnome-terminal --title="2: Gazebo Simulator" -- bash -c "\
     cd $ROOT_DIR && \
-    ./start_sim.sh; \
+    ./start_wheelchair_sim.sh worlds/reduced_living_room.sdf; \
     exec bash"
 
 sleep 4 # Give Gazebo a few seconds to boot up before trying to bridge its topics
 
-# Window 3: ROS 2 to Ignition Bridge
+# Window 3: ROS 2 to Gazebo Bridge
 gnome-terminal --title="3: ROS Bridge" -- bash -c "\
     source /opt/ros/humble/setup.bash && \
-    echo 'Bridging ROS 2 /cmd_vel to Ignition...'; \
-    ros2 run ros_gz_bridge parameter_bridge /cmd_vel@geometry_msgs/msg/Twist]ignition.msgs.Twist; \
+    echo 'Bridging ROS 2 /cmd_vel and /model/my_wheelchair/odometry...'; \
+    ros2 run ros_gz_bridge parameter_bridge /cmd_vel@geometry_msgs/msg/Twist]ignition.msgs.Twist /model/my_wheelchair/odometry@nav_msgs/msg/Odometry[ignition.msgs.Odometry
     exec bash"
 
 # Window 4: Mock Serial Sender
