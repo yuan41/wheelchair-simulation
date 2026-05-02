3b6a298ad5f1bfd87e7c6d668672d9f85dfd3c99
ennisc2 <ennisc2@illinois.edu>
2026-05-02 11:55:44 -0500

    csvtopy updated for plotting feature

---
+++ scripts/csvTopy.py
@@ -0,0 +1,34 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('./turtle1_cmd_vel.csv')
+
+# print(df)
+
+timestamp = df['timestamp'].to_numpy()
+linear_x  = df['linear.x'].to_numpy()
+linear_y  = df['linear.y'].to_numpy()
+linear_z  = df['linear.z'].to_numpy()
+angular_x = df['angular.x'].to_numpy()
+angular_y = df['angular.y'].to_numpy()
+angular_z = df['angular.z'].to_numpy()
+
+dt = 0.01 #Step Size
+t = np.linspace(start = 0, stop = dt*timestamp.shape, num= timestamp.shape, endpoint=True )
+    
+fig, axs = plt.subplots(3)
+fig.suptitle('Plots')
+axs[0].plot(t, linear_x)
+axs[0].set_title('Vx')
+axs[1].plot(t, linear_y)
+axs[1].set_title('Vy')
+axs[2].plot(t, angular_z)
+axs[2].set_title('Wz')
+
+# print(type(linear_x)) #numpy arrays
+
+print(f"Recorded {len(timestamp)} messages")
+print(f"Max linear speed:  {linear_x.max()}")
+print(f"Max linear speed:  {linear_y.max()}")
+print(f"Max angular speed: {angular_z.max()}")
